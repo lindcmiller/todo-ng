e236ef70023b842ed70b6e1053f1dbcbb2bde60e
Lindsey Miller <lindc.miller@gmail.com>
2015-11-10 15:56:17 -0500

    user can delete completed todos

--- todo.js
+++ todo.js
@@ -5,18 +5,18 @@ todoApp.controller('TodoController', function($scope) {
   $scope.todos = [];
 
   $scope.getTotalTodos = function() {
-    return $scope.todos.length;
-  };
+      return $scope.todos.length;
+    };
 
   $scope.addTodo = function() {
-    $scope.todos.push({text: $scope.todo, "completed": false});
-    $scope.todo = "";
-  };
+      $scope.todos.push({"text": $scope.todo, "completed": false});
+      $scope.todo = "";
+    };
 
-  $scope.clearCompleted = function(todos) {
-      if(todos.completed) {
-        $scope.todos.splice(todo, 1);
-      };
+  $scope.clearCompleted = function() {
+    $scope.todos = $scope.todos.filter(function(todo) {
+      return !todo.completed;
+    });
   };
 
 });
