a74f1b6f4dba3c97abf2c48cd09eaee82e2060bb
Lindsey Miller <lindc.miller@gmail.com>
2015-11-11 14:03:45 -0500

    poorly executed  methods

--- todo.js
+++ todo.js
@@ -1,17 +1,30 @@
 var todoApp = angular.module("todoApp", []);
 
-todoApp.controller('TodoController', function($scope) {
+todoApp.controller('TodoController', function($scope, $http) {
 
   $scope.todos = [];
+
+// need to change deprecated success/error methods
+  $http.get('/todos')
+    .then(function(todos) {
+      $scope.loaded = true;
+      $scope.todos = todos;
+    }, function(err) {
+      return alert(err)).error;
+    });
 
   $scope.getTotalTodos = function() {
       return $scope.todos.length;
     };
 
-  $scope.addTodo = function() {
-      $scope.todos.push({"text": $scope.todo, "completed": false, "editing": false});
+  $scope.addTodo = function(text) {
+    $http.post('/todos', function(todo) {
       $scope.todo = "";
-    };
+      $scope.todos.push({"text": $scope.todo, "completed": false, "editing": false});
+    }, function(err) {
+      return "Could not save this to-do.");
+    });
+  };
 
   $scope.editTodo = function(todo) {
     todo.editing = true;
@@ -19,12 +32,27 @@ todoApp.controller('TodoController', function($scope) {
 
   $scope.updateTodo = function(todo) {
     todo.editing = false;
-  }
+    $http.put('/todos/' + todo.id
+  }).then(function(todo) {
+      completed: todo.completed;
+    }, function(err) {
+      return alert(err.message || (err.errors && err.errors.completed || "Could not update this to-do.");
+    });
+  };
 
   $scope.clearCompleted = function() {
-    $scope.todos = $scope.todos.filter(function(todo) {
-      return !todo.completed;
-    });
+    $http.delete('/todos/' + todo.id, {
+      params: {
+        completed: true
+      }
+    }).then(function() {
+      $scope.todos = $scope.todos.filter(function(todo) {
+        return !todo.completed;
+      });
+    }, function(err) {
+      return alert(err.message || "Could not delete this to-do.");
+    })
+
   };
 
 });
