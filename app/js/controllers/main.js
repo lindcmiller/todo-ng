d7187813207ac59e510bf0283ea6725a14b22a7d
Lindsey Miller <lindc.miller@gmail.com>
2015-11-16 17:15:26 -0500

    user can add todo, user can edit todo - will update but not save

--- app/js/controllers/main.js
+++ app/js/controllers/main.js
@@ -1,53 +1,57 @@
+'use strict';
+
 var todoApp = angular.module("todoApp", []);
 
 todoApp.controller('TodoController', function($scope, $http) {
 
   $scope.todos = [];
 
-  $http.get('/api')
+  $http.get('/api/v1/todos')
     .then(function(response) {
       $scope.loaded = true;
       $scope.todos = response.data.todos;
-      debugger;
-    }, function(err) {
-
-      return err;
+    }, function() {
+      return "Cannot load to-dos.";
     });
 
-$scope.addTodo = function(title) {
-  $http.post('/todos', function(todo) {
-    $scope.todo = "";
-    $scope.todos.push({"title": $scope.todo, "is_completed": false, "editing": false});
-  }, function(err) {
-    return "Could not save this to-do.";
-  });
-};
+  $scope.addTodo = function() {
+    $http.post('/api/v1/todos', {
+      title: $scope.todo,
+      is_completed: false
+    }).then(function(response) {
+      $scope.todos.push(response.data.todo);
+      $scope.todo = "";
+    }, function(err) {
+      return "Could not add this to-do.";
+    });
 
-$scope.editTodo = function(todo) {
-  todo.editing = true;
-};
+  };
 
-$scope.updateTodo = function(todo) {
-  todo.editing = false;
-  $http.put('/todos/' + todo.id)
-    .catch(function(err) {
+  $scope.editTodo = function(todo) {
+    todo.editing = true;
+  };
+
+  $scope.updateTodo = function(todo) {
+    todo.editing = false;
+    $http.put('/api/v1/todos/' + todo.id, {
+      title: $scope.todo,
+      is_completed: false
+    }).catch(function(err) {
       return "Could not update this to-do.";
     });
-};
+  };
 
-$scope.clearCompleted = function() {
-  $http.delete('/todos/' + todo.id, {
-    params: {
-      is_completed: true
-    }
-  }).then(function() {
-    $scope.todos = $scope.todos.filter(function(todo) {
-      return !todo.is_completed;
+  $scope.clearCompleted = function() {
+    $http.delete('/api/v1/todos', {
+      params: {
+        is_completed: true
+      }}).then(function(todos) {
+      $scope.todos = $scope.todos.filter(function(todo) {
+        return !todo.is_completed;
+      });
+    }, function(err) {
+      return "Could not delete this to-do.";
     });
-  }, function(err) {
-    return "Could not delete this to-do.";
-  })
-
-};
+  };
 
 });
